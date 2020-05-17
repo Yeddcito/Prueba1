d1b40f7bb511494861d3ad7b38a155778b098c05
Yeddcito <62890419+Yeddcito@users.noreply.github.com>
2020-05-17 14:57:52 -0400

    Add files via upload

---
+++ Vehiculo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgenciaVehiculos
+{
+    abstract class Vehiculo : VehiculoComponentes
+    {
+        private Motor _motor;
+        private Rueda[] _ruedas;
+        
+        private Mezclador _mezclador;
+        private Automovil _automovil;
+        
+        
+        public Vehiculo(string marca, int año, int kilometraje, 
+                        int idMotor, TipoMotor tipoMotor, int cilindrada,
+                        int numRuedas, TipoRecubrimiento tipoRecubrimiento,
+                        int minDurometro, int maxDurometro,
+                        TipoMezclador tipoMezclador, double capacidad)
+        {
+            _motor = new Motor(idMotor, tipoMotor, cilindrada);
+            _ruedas = new Rueda[numRuedas];
+            for(int i = 0;i < numRuedas;i++)
+            {
+                Ruedas[i] = new Rueda(tipoRecubrimiento, minDurometro, maxDurometro);
+            }
+
+
+            _mezclador = new Mezclador(tipoMezclador);
+
+        }
+
+        internal Motor Motor { get => _motor; set => _motor = value; }
+        internal Rueda[] Ruedas { get => _ruedas; set => _ruedas = value; }
+        internal Mezclador Mezclador { get => _mezclador; set => _mezclador = value; }
+        internal Automovil Automovil { get => _automovil; set => _automovil = value; }
+    }
+}
